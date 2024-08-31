1c5699c245e02aa64311fd5b70cb15f70e31b983
bn-merwane <m.bensalah@esi-sba.dz>
2024-08-31 20:20:33 +0100

    .

--- event/settings.py
+++ event/settings.py
@@ -10,12 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!0-z=%jxagrhnc58*z64bq_cb+)6k5xf#98jo#k@ys0dhl2_d*'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
